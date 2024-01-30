0720e2e460afb4b6c375a36ae8d3717d0f4affa4
Szymon Dębski <szymon.debski@vee.ai>
2024-01-30 13:08:47 +0100

    added email-service base, fixed nginx

--- users/entrypoint.sh
+++ users/entrypoint.sh
@@ -13,4 +13,4 @@ python manage.py create_admin
 python manage.py collectstatic --no-input
 # python manage.py runserver
 
-gunicorn docker_django.wsgi:application --bind 0.0.0.0:8000 --reload
+gunicorn users.wsgi:application --bind 0.0.0.0:8000 --reload

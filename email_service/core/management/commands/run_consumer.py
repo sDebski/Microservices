63ddd37e9938890daa3c740ba42b49d8c3564744
Szymon Dębski <skwdebski@gmail.com>
2024-01-30 13:33:02 +0100

    docker compose modified - added mq-consumer base

---
+++ email_service/core/management/commands/run_consumer.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+from email_service.consumer import Consumer
+
+
+class Command(BaseCommand):
+    help = "Starts AMQP consumer."
+
+    def _run_consumer(self, consumer):
+        print('Run a consumer')
+
+    def handle(self, *args, **options):
+        self.stdout.write("Starting AMQP consumer...", self.style.WARNING)
+
+        consumer = Consumer()
+        self._run_consumer(consumer)

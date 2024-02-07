2e95c9ad059ebdaa5f7ef1ce617bcaa082a0fc8e
Szymon Dębski <szymon.debski@vee.ai>
2024-02-07 15:10:13 +0100

    added mq consumer

--- email_service/email_service/consumer.py
+++ email_service/email_service/consumer.py
@@ -1,4 +1,36 @@
+import json
 import pika
+from django.conf import settings
+
 
 class Consumer:
-    pass
+    QUEUES_CALLBACKS = {
+        "email_service_queue": "get_email_data",
+    }
+
+    def __init__(self):
+        self._params = pika.URLParameters(settings.MQ_URL)
+        self._conn = None
+        self._channel = None
+
+    def connect(self):
+        if not self._conn and self._conn.is_closed:
+            print(f"Connecting to {settings.MQ_URL}")
+            self._conn = pika.BlockingConnection(self._params)
+            self._channel = self._conn.channel()
+    
+    def get_email_data(self, channel, method, properties, body):
+
+        decoded = body.decode()
+        message = json.loads(decoded).values()
+
+        print(message)
+    
+    def _bind_queues_to_callback(self):
+        for queue, callback in self.QUEUES_CALLBACKS.items():
+            self._channel.queue_declare(queue=queue)
+            self._channel.basic_consume(queue=queue, on_message_callback=getattr(self, callback), auto_ack=True)
+
+    def consume(self):
+        self._bind_queues_to_callback()
+        self._channel.start_consuming()
